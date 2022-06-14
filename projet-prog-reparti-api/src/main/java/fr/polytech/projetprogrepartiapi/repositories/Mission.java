e7ef5f8bf73b44fb985cc004c8fc5fa538196272
Asigan <40575190+Asigan@users.noreply.github.com>
2022-06-14 14:01:28 +0200

    Correction MissionRepository

--- projet-prog-reparti-api/src/main/java/fr/polytech/projetprogrepartiapi/repositories/Mission.java
+++
@@ -1,7 +0,0 @@
-package fr.polytech.projetprogrepartiapi.repositories;
-
-import fr.polytech.projetprogrepartiapi.entities.Indicator;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface Mission extends JpaRepository<Mission, Integer> {
-}
