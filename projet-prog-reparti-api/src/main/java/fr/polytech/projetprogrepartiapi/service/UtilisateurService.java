4d61f5ea18861886067ecd826403496f6099934b
Asigan <40575190+Asigan@users.noreply.github.com>
2022-06-14 14:10:57 +0200

    Service Utilisateur de base

--- projet-prog-reparti-api/src/main/java/fr/polytech/projetprogrepartiapi/service/UtilisateurService.java
+++ projet-prog-reparti-api/src/main/java/fr/polytech/projetprogrepartiapi/service/UtilisateurService.java
@@ -1,2 +1,24 @@
-package fr.polytech.projetprogrepartiapi.service;public class UtilisateurService {
+package fr.polytech.projetprogrepartiapi.service;
+
+import fr.polytech.projetprogrepartiapi.entities.Utilisateur;
+import fr.polytech.projetprogrepartiapi.repositories.UtilisateurRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UtilisateurService {
+    private final UtilisateurRepository utilisateurRepository;
+
+    public UtilisateurService(UtilisateurRepository utilisateurRepository){
+        this.utilisateurRepository = utilisateurRepository;
+    }
+
+    public Optional<Utilisateur> getUtilisateurById(int id){
+        return utilisateurRepository.findById(id);
+    }
+
+    public boolean utilisateurExists(int id){
+        return utilisateurRepository.existsById(id);
+    }
 }
