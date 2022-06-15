ea0a660887373919393270c51642e9c00e70b96b
Yaimuu <yanis.ouled@gmail.com>
2022-06-15 16:08:16 +0200

    Login update

--- projet-prog-reparti-api/src/main/java/fr/polytech/projetprogrepartiapi/controller/AuthController.java
+++ projet-prog-reparti-api/src/main/java/fr/polytech/projetprogrepartiapi/controller/AuthController.java
@@ -1,5 +1,9 @@
 package fr.polytech.projetprogrepartiapi.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.util.JSONPObject;
+import fr.polytech.projetprogrepartiapi.entities.LoginRequest;
+import fr.polytech.projetprogrepartiapi.entities.Utilisateur;
 import fr.polytech.projetprogrepartiapi.repositories.UtilisateurRepository;
 import fr.polytech.projetprogrepartiapi.service.AuthentificationService;
 import fr.polytech.projetprogrepartiapi.service.UtilisateurService;
@@ -15,6 +19,9 @@ import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 @RestController
@@ -32,23 +39,51 @@ public class AuthController {
     public ResponseEntity<Object> login(HttpServletRequest request, HttpServletResponse response) {
         logger.info("GET login");
 
+        String message = "";
+
         if (!"POST".equalsIgnoreCase(request.getMethod()))
             return new ResponseEntity("Only POST requests are allowed !", HttpStatus.BAD_REQUEST);
 
         UtilisateurService uService = new UtilisateurService(utilisateurRepository);
-        String test = "";
+        String requestString = "";
+        LoginRequest loginRequest = new LoginRequest();
+        Utilisateur utilisateur = null;
+        Map<String, Object> mappedRequest = new HashMap<>();
+
+        HttpSession session;
         try {
-            test = request.getReader().lines().collect(Collectors.joining(System.lineSeparator()));
+            requestString = request.getReader().lines().collect(Collectors.joining(System.lineSeparator()));
+            ObjectMapper mapper = new ObjectMapper();
+            mappedRequest = mapper.readValue(requestString, Map.class);
+
+            loginRequest.setNomUtil((String) mappedRequest.get("login"));
+            loginRequest.setMotPasse((String) mappedRequest.get("password"));
+
+            message = "Tentative de connexion au compte de " + loginRequest.getNomUtil() + "... ";
+            logger.info((String) mappedRequest.get("login"));
+            logger.info(message);
+            logger.debug(loginRequest.toString());
+
+            //TODO: Login
+            utilisateur = authentificationService.authentification( loginRequest );
+
+            if (utilisateur != null) {
+                message = "Mot de passe correct";
+                session = request.getSession();
+                session.setAttribute("id", utilisateur.getNumUtil());
+            } else {
+                message = "Mot de passe incorrect";
+                request.setAttribute("message", message);
+                return new ResponseEntity("Incorrect login or password !", HttpStatus.FORBIDDEN);
+            }
+
         } catch (Exception e) {
             e.printStackTrace();
         }
 
-        logger.info(test);
+//        logger.info(requestString);
+        logger.info(message);
 
-        //TODO: Login
-
-
-        return ResponseEntity.ok(test);
-//        return ResponseEntity.ok(uService.getUtilisateurById());
+        return ResponseEntity.ok(utilisateur);
     }
 }
