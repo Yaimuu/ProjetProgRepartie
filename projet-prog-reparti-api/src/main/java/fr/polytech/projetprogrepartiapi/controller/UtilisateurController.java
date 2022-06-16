4b4c8ffb32dbe2d66796618bd6d283c0a9ffdaf8
Asigan <40575190+Asigan@users.noreply.github.com>
2022-06-16 20:12:47 +0200

    Ajout route user/{id}/inscriptions

--- projet-prog-reparti-api/src/main/java/fr/polytech/projetprogrepartiapi/controller/UtilisateurController.java
+++ projet-prog-reparti-api/src/main/java/fr/polytech/projetprogrepartiapi/controller/UtilisateurController.java
@@ -1,8 +1,10 @@
 package fr.polytech.projetprogrepartiapi.controller;
 
 
+import fr.polytech.projetprogrepartiapi.entities.Inscription;
 import fr.polytech.projetprogrepartiapi.entities.Utilisateur;
 import fr.polytech.projetprogrepartiapi.repositories.UtilisateurRepository;
+import fr.polytech.projetprogrepartiapi.service.InscriptionService;
 import fr.polytech.projetprogrepartiapi.service.UtilisateurService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,6 +14,8 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -37,6 +41,7 @@ public class UtilisateurController {
             if(uService.isAdmin((int) session.getAttribute("id")))
                 return ResponseEntity.ok(uService.getAllUtilisateurs());
         }
+        logger.info("error");
 
         return new ResponseEntity(HttpStatus.FORBIDDEN);
     }
@@ -75,6 +80,35 @@ public class UtilisateurController {
 
                 uService.deleteUtilisateur(utilisateurToRemove.get());
                 return ResponseEntity.ok(uService.getUtilisateurById(id));
+            }
+        }
+
+        return new ResponseEntity(HttpStatus.FORBIDDEN);
+    }
+
+    @GetMapping("/api/user/{id}/inscriptions")
+    public ResponseEntity<Object> getInscriptionsForUser(@PathVariable int id, HttpServletRequest request) {
+        logger.info("GET user/" +id+"/inscriptions" );
+
+        HttpSession session = request.getSession();
+        UtilisateurService uService = new UtilisateurService(utilisateurRepository);
+        Utilisateur u = uService.getAutenticatedUtilisateur(request);
+        if(u != null)
+        {
+            logger.info("Authentied user found");
+            if(uService.isAdmin(u.getNumUtil()) || u.getNumUtil() == id){
+                logger.info("Authorization granted");
+                Optional<Utilisateur> uconcerne = uService.getUtilisateurById(id);
+                if(uconcerne.isPresent()){
+                    logger.info("User found");
+                    List<Inscription> inscriptionsUser = uService.getAllInscriptionsFromUser(uconcerne.get());
+                    List<Inscription> inscriptions = new ArrayList<>();
+                    for(Inscription i: inscriptionsUser){
+                        logger.info(i.getMissionByFkMission().getWording());
+                        inscriptions.add(i);
+                    }
+                    return ResponseEntity.ok(inscriptions);
+                }
             }
         }
 
